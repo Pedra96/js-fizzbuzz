773f1bb2f0eb399334d39096bc0bb4a6f8208208
Pedra96 <darkraidialga@yahoo.it>
2022-11-11 15:42:16 +0100

    grafica migliorata

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 let container=document.querySelector(".myContainer");
-
 for(let i=1;i<=100;i++){
     if(i%3==0 && i%5==0){
         container.innerHTML += '<div class="elemento"><p>FizzBuzz</p></div>';
